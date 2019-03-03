9bae70e88a63057c7a1f41cfe83e34c0987ee666
ptsouth97 <ptsouth97@gmail.com>
2019-03-03 15:27:20 +0000

    modified motor.py

--- motor.py
+++ motor.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 # so that script can be run from Brickman
 
-from ev3dev.ev3 import *
+import ev3dev.ev3 as ev3
 from time import sleep
 
 
 def main():
 	''' main function for testing purposes'''
 
-	m = LargeMotor('outB')
+	# instantiate a motor object on the 'B' port
+	m = ev3.LargeMotor('outB')
 	
 	#turn_360(m)
 	backwards(m)
@@ -19,10 +20,27 @@ def turn_360(m):
 	'''makes a large motor on port B turn through 360° at speed 900 
 	   and optionally apply a 'hold' (like a strong brake - see later):'''
 
-	m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
+	# run_to_rel_pos runs to a position relative to the current position value
 
-	 # Give the motor time to move'''
-	sleep(5)
+	# position_sp specifies the target position for run_to_rel_pos command
+	position_sp=360
+
+	# speed_sp sets the target speed in tacho counts per second
+	speed_sp=900
+
+	# stop actions = coast, brake, or hold
+	stop_action='hold'
+
+	m.run_to_rel_pos(position_sp=position_sp, speed_sp=speed_sp, stop_action=stop_action)
+
+	# Wait for completion
+	m.wait_while('running')
+
+	# Beep when completed
+	ev3.Sound.beep()
+
+	# Alternatively, give the motor time to move
+	#sleep(5)
   
 	return
 
@@ -32,11 +50,22 @@ def backwards(m):
 	3 seconds with a 'speed setpoint' set to -750 (equivalent to a power 
 	setting of -75 in the standard EV3 software).'''
 
-	m.run_timed(time_sp=3000, speed_sp=-750)
+	# run_timed runs the motor for the amount of time specified
+
+	# time_sp is amount of time motor will run in milliseconds
+	time_sp=3000
+
+	# speed_sp sets the target speed in tacho counts per second ('-' for backwards)
+	speed_sp=-750
+
+	m.run_timed(time_sp=time_sp, speed_sp=speed_sp)
 	print("set speed (speed_sp) = " + str(m.speed_sp))
 	sleep(1)  # it takes a moment for the motor to start moving
 	print("actual speed = " + str(m.speed))
-	sleep(4)
+	m.wait_while('running')
+	#sleep(4)
+
+	ev3.Sound.beep()
 
 	return
 
@@ -48,6 +77,8 @@ def turn_forever(m):
 	sleep(5)
 	m.stop(stop_action="hold")
 	sleep(5)
+
+	ev3.Sound.beep()
 
 	return
 
