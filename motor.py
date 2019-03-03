7f8966d9d37a52bb624e4bc8e08cb9eca2e3288d
ptsouth97 <ptsouth97@gmail.com>
2019-03-03 14:50:06 +0000

    defined functions in motor.py

--- motor.py
+++ motor.py
@@ -5,32 +5,52 @@ from ev3dev.ev3 import *
 from time import sleep
 
 
-'''makes a large motor on port B turn through 360° at speed 900 
-and optionally apply a 'hold' (like a strong brake - see later):'''
-'''
-m = LargeMotor('outB')
+def main():
+	''' main function for testing purposes'''
 
-m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
+	m = LargeMotor('outB')
+	
+	#turn_360(m)
+	backwards(m)
+	#turn_forever(m)
 
-sleep(5)   # Give the motor time to move'''
+
+def turn_360(m):
+	'''makes a large motor on port B turn through 360° at speed 900 
+	   and optionally apply a 'hold' (like a strong brake - see later):'''
+
+	m.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
+
+	 # Give the motor time to move'''
+	sleep(5)
+  
+	return
 
 
-'''This example runs a large motor attached to port B backwards for 
-3 seconds with a 'speed setpoint' set to -750 (equivalent to a power 
-setting of -75 in the standard EV3 software).'''
-'''
-m = LargeMotor('outB')
-m.run_timed(time_sp=3000, speed_sp=-750)
-print("set speed (speed_sp) = " + str(m.speed_sp))
-sleep(1)  # it takes a moment for the motor to start moving
-print("actual speed = " + str(m.speed))
-sleep(4)'''
+def backwards(m):
+	'''This example runs a large motor attached to port B backwards for 
+	3 seconds with a 'speed setpoint' set to -750 (equivalent to a power 
+	setting of -75 in the standard EV3 software).'''
 
-'''turn motor forever'''
+	m.run_timed(time_sp=3000, speed_sp=-750)
+	print("set speed (speed_sp) = " + str(m.speed_sp))
+	sleep(1)  # it takes a moment for the motor to start moving
+	print("actual speed = " + str(m.speed))
+	sleep(4)
 
-m = LargeMotor('outB')
+	return
 
-m.run_forever(speed_sp=900)
-sleep(5)
-m.stop(stop_action="hold")
-sleep(5)
+
+def turn_forever(m):
+	'''turn motor forever'''
+
+	m.run_forever(speed_sp=900)
+	sleep(5)
+	m.stop(stop_action="hold")
+	sleep(5)
+
+	return
+
+
+if __name__ == '__main__':
+	main()
