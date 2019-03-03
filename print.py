62985107394c0738de174507aa5e06152f371cff
ptsouth97 <ptsouth97@gmail.com>
2019-03-03 16:49:13 +0000

    updated print.py

--- print.py
+++ print.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+import ev3dev.ev3 as ev3
 from time import sleep
 import os
 
-os.system('setfont Lat15-TerminusBold32x16')
 
-print('EV3 Python rules!')
-sleep(15)
+def main():
+	''' Main function'''
+
+	os.system('setfont Lat15-TerminusBold32x16')
+
+	print('EV3 Python rules!')
+	sleep(5)
+	ev3.Sound.beep()
+
+
+if __name__ == '__main__':
+	main()
