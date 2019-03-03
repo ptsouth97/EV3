9127acc16ea373afef9edabd5caae62024bec760
ptsouth97 <ptsouth97@gmail.com>
2019-03-03 16:35:12 +0000

    updated ir.py

--- ir.py
+++ ir.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
-# so that script can be run from Brickman
 
-from ev3dev.ev3 import *
+import ev3dev.ev3 as ev3
 
-# Connect infrared and touch sensors to any sensor ports
-# and check they are connected.
 
-ir = InfraredSensor() 
-assert ir.connected, "Connect a single infrared sensor to any sensor port"
+def main():
+	''' Main function for testing purposes'''
 
-ts = TouchSensor();    assert ts.connected, "Connect a touch sensor to any port" 
-# can have 2 statements on same line if use semi colon
+	ir = ev3.InfraredSensor()
+	sensor(ir)
 
-# Put the infrared sensor into proximity mode.
-ir.mode = 'IR-PROX'
 
-while not ts.value():    # Stop program by pressing touch sensor button
-    # Infrared sensor in proximity mode will measure distance to the closest
-    # object in front of it.
-    distance = ir.value()
+def sensor(ir):
+	'''Connect infrared and touch sensors to any sensor ports and check they are connected'''
 
-    if distance < 60:
-        Leds.set_color(Leds.LEFT, Leds.RED)
-    else:
-        Leds.set_color(Leds.LEFT, Leds.GREEN)
+	assert ir.connected, "Connect a single infrared sensor to any sensor port"
 
-Sound.beep()       
-Leds.set_color(Leds.LEFT, Leds.GREEN)  
-#make sure left led is green before exiting
+	#ts = TouchSensor();    assert ts.connected, "Connect a touch sensor to any port" 
+	# can have 2 statements on same line if use semi colon
+
+	# Put the infrared sensor into proximity mode.
+	ir.mode = 'IR-PROX'
+	'''
+	while not ts.value():    # Stop program by pressing touch sensor button
+	# Infrared sensor in proximity mode will measure distance to the closest
+	# object in front of it.'''
+
+	distance = ir.value()
+
+	if distance < 60:
+		ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+
+	else:
+		ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+
+	ev3.Sound.beep()       
+	ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)  
+	#make sure left led is green before exiting
+	
+	return
+
+
+if __name__ == '__main__':
+	main()
