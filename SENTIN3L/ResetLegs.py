66dd561a40e82f688b3d9bc6ba736445231b377b
ptsouth97 <ptsouth97@gmail.com>
2019-04-19 22:31:58 +0000

    added SENTIN3L

---
+++ SENTIN3L/ResetLegs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from time import sleep
+from ev3dev.ev3 import *
+
+
+def main():
+	''' main function'''
+
+	ResetLegs()
+
+
+def ResetLegs():
+	''' resets legs'''
+
+	lcd = Screen()
+	lcd.draw.
+
+	sleep(1)
+
+	return	
+
+
+
+if __name__ == '__main__':
+	main()
