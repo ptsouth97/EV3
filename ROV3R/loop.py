9be5b78bd1f7013e715f3a5958bef826b08c5078
Blake Crosby <ptsouth97@gmail.com>
2020-02-11 22:54:55 -0500

    changed motor control to MoveTank

--- ROV3R/loop.py
+++ ROV3R/loop.py
@@ -8,17 +8,28 @@ def main():
 
 	mB = ev3.LargeMotor('outB')
 	mC = ev3.LargeMotor('outC')
+	
+	tank_drive = MoveTank(mB, mC)
+	run(tank_drive)
 
-	run(mB, mC)
 
-
-def run(mB, mC):
+def run(tk_drive):
 	''' run loop'''
 
+	
+	
 	while True:
-
-		mB.run_forever(speed_sp=900)
-
-
+		
+		# MOVE FORWARD
+		# mB.run_forever(speed_sp=900)
+		tk_drive.on(50, 50)	
+		
+		# CHECK IR SENSOR
+				
+		# IF IR SENSORY PROXIMITY < 25, BACK UP TO THE RIGHT
+		
+		# WAIT 0.25 SEC
+		
+		
 if __name__ == '__main__':
 	main()
