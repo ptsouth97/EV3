3e5716f3f15887d20af12aa19807d6c8c8b3695a
ptsouth97 <ptsouth97@gmail.com>
2020-02-17 17:46:57 +0000

    cleaned up move tank

--- ROV3R/loop.py
+++ ROV3R/loop.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 
-<<<<<<< HEAD
-from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank
+from ev3dev2.motor import OUTPUT_B, OUTPUT_C, MoveTank
+
 from time import sleep
 
 
 def main():
 	''' main function'''
 
-<<<<<<< HEAD
-	# mB = ev3.LargeMotor('outB')
-	# mC = ev3.LargeMotor('outC')
-
-	# run(mB, mC)
-	
-	tank_drive = MoveTank('outB', 'outC')
+	tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
 	run(tank_drive)
+
 
 def run(tank):
 	''' run loop'''
 
-	# mB.run_timed(time_sp=5, speed_sp=900, stop_action='brake')
-	# mB.run_forever(speed_sp=900)
 	tank.on_for_seconds(100, 100, 5, brake=True, block=True)
 
 	return
