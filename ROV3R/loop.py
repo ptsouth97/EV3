26ef323268f27bb9d1c7bfd7f332e82b13516e4d
ptsouth97 <ptsouth97@gmail.com>
2020-02-17 17:21:20 +0000

    added move tank for ev3dev2

--- ROV3R/loop.py
+++ ROV3R/loop.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 
-import ev3dev.ev3 as ev3
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank
+#import ev3dev.ev3 as ev3
+#from ev3dev.motor import MoveTank
 
 
 def main():
 	''' main function'''
 
-	mB = ev3.LargeMotor('outB')
-	mC = ev3.LargeMotor('outC')
+	# mB = ev3.LargeMotor('outB')
+	# mC = ev3.LargeMotor('outC')
 
-	run(mB, mC)
+	# run(mB, mC)
+	
+	tank_drive = MoveTank('outB', 'outC')
+	run(tank_drive)
 
-
-def run(mB, mC):
+def run(tank):
 	''' run loop'''
 
-	while True:
+	# mB.run_timed(time_sp=5, speed_sp=900, stop_action='brake')
+	# mB.run_forever(speed_sp=900)
+	tank.on_for_seconds(100, 100, 5, brake=True, block=True)
 
-		mB.run_forever(speed_sp=900)
+	return
 
 
 if __name__ == '__main__':
