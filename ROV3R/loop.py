938bcc5051ba5e67ff8996d22e2b40e37ab7e23e
ptsouth97 <ptsouth97@gmail.com>
2020-02-21 03:00:37 +0000

    added comments to code

--- ROV3R/loop.py
+++ ROV3R/loop.py
@@ -3,14 +3,12 @@
 from ev3dev2.motor import OUTPUT_B, OUTPUT_C, MoveTank
 from ev3dev2.sensor.lego import InfraredSensor
 from ev3dev2.sound import Sound
-from time import sleep
 
 
 def main():
 	''' main function'''
 
 	tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
-	#run(tank_drive)
 
 	infrared = InfraredSensor()
 	infrared.mode = 'IR-PROX'
@@ -25,18 +23,18 @@ def run(tank, ir, sound):
 
 	while True:
 
+		# Check ir sensor for proximity to objects
 		distance = ir.value()
-		#print("The distance is " + str(distance))
 
+		# Drive ROV3R forward
 		tank.on(100, 100)
 
+		# If proximity is less than 60, beep and back up to the right for 1/4 second
 		if distance < 60:
 			sound.beep()
 			tank.on_for_seconds(-100, 0, 0.25)
-			#time.sleep(0.25)
 			continue
 
-		#tank.on_for_seconds(100, 100, 5, brake=True, block=True)
 
 	return
 		
